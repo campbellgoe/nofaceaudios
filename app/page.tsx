81128b79823dbf3cc27a3d0cfccaeb437d7fa2ef
George Campbell <campbell.goe@gmail.com>
2024-04-10 20:56:57 +0100

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to <Link href="https://nofaceaudios.vercel.app">https://nofaceaudios.vercel.app</Link></h1>
-      {Audios.map(audio => <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      {Audios.map(audio => <div key={audio.src} className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <label>Sintax, Mk1 <audio controls src={audio.src} /></label>
       </div>)}
     </main>
